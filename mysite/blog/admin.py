5c04aafd88e42d68f4c3330edac4a55bcf2a3f6a
blackjohnrose <18.mamali@gmail.com>
2021-07-26 17:46:32 +0430

    customizing the display of admin panel

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,4 +3,17 @@ from .models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+# Show all models
+# admin.site.register(Post)
+
+# Customizing model display
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('title', 'slug', 'author', 'publish', 'status')
+  list_filter = ('title', 'created', 'publish', 'author')
+  search_fields = ('title', 'body')
+  prepopulated_fields = {'slug': ('title', )}
+  raw_id_fields = ('author',)
+  date_hierarchy = 'publish'
+  ordering = ('status', 'publish')
