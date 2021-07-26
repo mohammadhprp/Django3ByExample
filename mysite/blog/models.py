5632467986088c804e50bacd3ff3b4b037d67571
blackjohnrose <18.mamali@gmail.com>
2021-07-26 17:27:04 +0430

    fix typo

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -13,13 +13,13 @@ class Post(models.Model):
   slug = models.SlugField(max_length=250)
   author = models.ForeignKey(User,  on_delete=models.CASCADE, related_name='blog_posts')
   body = models.TextField()
-  publish = models.DateTimeFiled(default=timezone.now)
-  created = models.DateTimeFiled(auto_now_add=True)
-  updated = models.DateTimeFiled(auto_now=True)
+  publish = models.DateTimeField(default=timezone.now)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
   status = models.CharField(max_length=250, choices=STATUS_CHOICES, default='draft')
 
   class Meta:
-    ordering = ('-publish')
+    ordering = ('-publish',)
   
   def __str__(self):
     return self.title
