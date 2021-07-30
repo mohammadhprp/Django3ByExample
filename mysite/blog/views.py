e38bf6f79bfb38cf565829e6dc8cba136985d3e4
blackjohnrose <18.mamali@gmail.com>
2021-07-30 18:29:14 +0430

    create post detail funcation

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -11,3 +11,14 @@ def post_list(request):
   return render(request, 'blog/post/list.html', context)
 
 
+def post_detail(request, year, month, day, post):
+  post = get_object_or_404(Post, slug=post,
+                        status='published',
+                        publish__year=year,
+                        publish__month=month,
+                        publish__day=day)
+  context = {
+    'posts': posts,
+  }
+  
+  return render(request, 'blog/post/detail.html', context)
