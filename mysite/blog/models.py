d6c211ee96b70ed1f23d189be5f0801f44d64bae
blackjohnrose <18.mamali@gmail.com>
2021-07-30 18:46:47 +0430

    [fix bug] typo!

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
-class PublishedManager(models.Model):
+class PublishedManager(models.Manager):
   def get_queryset(self):
     return super(PublishedManager, self).get_queryset().filter(status='published')
 
