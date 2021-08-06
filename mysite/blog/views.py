e38cfddde3623f37bc493fe6e4889589d64b7560
Mohammad <18.mamali@gmail.com>
2021-08-06 19:01:47 +0430

    adding send mail to post share fun

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.views.generic import ListView
+
 
 from .models import Post
 from .forms import EmailPostForm
@@ -51,16 +53,23 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
   post = get_object_or_404(Post,  id=post_id, status='published')
+  sent = False
 
   if request.method == 'POST':
     form = EmailPostForm|(request>POST)
     if form.is_valid():
       cd = form.cleaned_data
+      post_url = request.build_absolute_url(post.get_absolute_url())
+      subject = f"{cd['name']} recommends you read {post.title}"
+      message = f"Read {post.title} at {post.url} \n\n {cd['name']}\'s comments: {cd['comments']}"
+      send_mail(subject, message, 'webinfowebpage@gmail.com', [cd['to']])
+      sent = True
   else:
     form = EmailPostForm()
 
   context = {
     'post': post,
-    'form': form
+    'form': form,
+    'sent': sent
   }
-  return render(request, 'blog/post/share.html' context)
+  return render(request, 'blog/post/share.html', context)
