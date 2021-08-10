693aec51acb0fceeebbc8a16485bae3521aedca9
Mohammad <18.mamali@gmail.com>
2021-08-10 17:47:24 +0430

    fix typo tag -> tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -24,7 +24,7 @@ def post_list(request, tag_slug=None):
 
   if tag_slug:
     tag = get_object_or_404(Tag, slug=tag_slug)
-    object_list = object_list.filter(tag__in=[tag])
+    object_list = object_list.filter(tags__in=[tag])
 
   paginator = Paginator(object_list, 3) # 3 posts in each page
   page = request.GET.get('page')
