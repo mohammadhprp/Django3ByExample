8cfb7095e4ba00465bd7b79a4052c37fbf9570fb
Mohammad <18.mamali@gmail.com>
2021-08-13 21:43:03 +0430

    add similar post in detail view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
+from django.db.models import Count
 
 
 from .models import Post, Comment
@@ -23,19 +24,19 @@ def post_list(request, tag_slug=None):
   tag = None
 
   if tag_slug:
-    tag = get_object_or_404(Tag, slug=tag_slug)
-    object_list = object_list.filter(tags__in=[tag])
+      tag = get_object_or_404(Tag, slug=tag_slug)
+      object_list = object_list.filter(tags__in=[tag])
 
   paginator = Paginator(object_list, 3) # 3 posts in each page
   page = request.GET.get('page')
-
   try:
-    posts = paginator.page(page)
+      posts = paginator.page(page)
   except PageNotAnInteger:
-    # if page not an integer deliver the first page
-    posts = paginator.page(1)
+      # If page is not an integer deliver the first page
+      posts = paginator.page(1)
   except EmptyPage:
-    posts = paginator.page(paginator.num_pages)
+      # If page is out of range deliver last page of results
+      posts = paginator.page(paginator.num_pages)
 
   context = {
     'page': page,
@@ -66,11 +67,19 @@ def post_detail(request, year, month, day, post):
   else:
     comment_form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags','-publish')[:4]
+
+
   context = {
     'post': post,
     'comments': comments,
     'new_comment': new_comment,
     'comment_form': comment_form,
+    'similar_posts': similar_posts
   }
 
   return render(request, 'blog/post/detail.html', context)
