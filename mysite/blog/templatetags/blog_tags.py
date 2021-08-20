d4a4a026c3fe63ebb373f368f1bf1ac31edb9d20
Mohammad <18.mamali@gmail.com>
2021-08-20 17:44:26 +0430

    add markdown filter

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,8 @@
-from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+from django import template
+import markdown
+
 from ..models import Post 
 
 register = template.Library()
@@ -17,3 +20,8 @@ def show_lastes_posts(count=5):
 @register.simple_tag
 def get_most_commented_posts(count=5):
   return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+  return mark_safe(markdown.markdown(text))
