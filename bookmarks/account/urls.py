3819e9e8024652f33fc28c66f2fd227b68fbec8e
Mohammad <18.mamali@gmail.com>
2021-08-30 20:17:43 +0430

    add path change and reset password

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,16 +1,34 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth.views import( 
     LoginView,
     LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
     )
 from .views import (
     user_login,
     dashboard
     )
 
+# app_name = 'account'
+
 urlpatterns = [
     # path('login/', user_login, name='login')
     path('', dashboard, name='dashboard'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    # change password urls
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # reset password
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
