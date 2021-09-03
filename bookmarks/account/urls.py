2fafa99184907621a388573c38a703d9e1369b8d
Mohammad <18.mamali@gmail.com>
2021-09-03 19:59:42 +0430

    Completed user profile

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -13,6 +13,7 @@ from .views import (
     dashboard,
     register,
     user_login,
+    edit,
     )
 
 # app_name = 'account'
@@ -37,5 +38,8 @@ urlpatterns = [
     path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    # profile edit
+    path('edit/', edit, name='edit')
 
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm, UserRegistrationForm
+from .models import Profile
+from .forms import (LoginForm,
+   UserRegistrationForm,
+   ProfileEditForm,
+   UserEditForm,
+ )
 
 
 def user_login(request):
@@ -38,7 +43,9 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(
                 user_form.cleaned_data['password'])
+
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -59,4 +66,28 @@ def dashboard(request):
 
   return render(request, 'account/dashboard.html', context)
 
+
+
+@login_required
+def edit(request):
+  if request.method == 'POST':
+    user_form = UserEditForm(instance=request.user, data=request.POST)
+    profile_form = ProfileEditForm(
+      instance=request.user.profile,
+      data=request.POST,
+      files=request.FILES,
+      )
+    if user_form.is_valid() and profile_form.is_valid():
+      user_form.save()
+      profile_form.save()
+  else:
+      user_form = UserEditForm(instance=request.user)
+      profile_form = ProfileEditForm(instance=request.user.profile)
+
+  context = {
+    'user_form': user_form,
+    'profile_form': profile_form
+  }
+
+  return render(request, 'account/edit.html', context)
 
