23d0f52c818773d55ae9e089f4628e9a5b64ec7d
Mohammad <18.mamali@gmail.com>
2021-09-17 19:12:34 +0430

    creating a detail view for images

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .forms import ImageCreateForm
+from .models import Image
 
 @login_required
 def image_create(request):
@@ -28,3 +29,12 @@ def image_create(request):
 
   return render(request, 'images/image/create.html', context)
 
+def image_detail(request, id, slug):
+  image = get_object_or_404(Image, id=id, slug=slug)
+
+  context = {
+    'section': 'images',
+    'image': image
+  }
+
+  return render(request, 'images/image/detail.html', context)
