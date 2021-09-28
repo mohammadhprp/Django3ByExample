3bcfd081a888264a89a1bc8c7b99c51b1cf55003
Mohammad <18.mamali@gmail.com>
2021-09-28 20:10:00 +0330

    creating a user list, and user detail views

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 from .models import Profile
 from .forms import (LoginForm,
@@ -97,3 +98,27 @@ def edit(request):
 
   return render(request, 'account/edit.html', context)
 
+
+@login_required
+def user_list(request):
+  users = User.objects.filter(is_active=True)
+
+  context = {
+    'section': 'people',
+    'users': users
+  }
+
+  return render(request, 'account/user/list.html', context)
+
+
+@login_required
+def user_detail(request, username):
+  user = get_object_or_404(User,
+                          username=username,
+                          is_active=True)
+  context = {
+    'section': 'people',
+    'user': user
+  }
+
+  return render(request, 'account/user/detail.html', context)
