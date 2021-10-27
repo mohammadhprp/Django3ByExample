a68308ba503f7041142d46257a7ff447a4c17417
Mohammad <18.mamali@gmail.com>
2021-10-27 10:18:07 +0330

    add path images_ranking views

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -3,13 +3,15 @@ from .views import (
     image_create,
     image_detail,
     image_like,
-    image_list
+    image_list,
+    image_ranking,
   )
 
 app_name='images'
 
 urlpatterns = [
   path('', image_list, name='list'),
+  path('ranking/', image_ranking, name='ranking'),
   path('create/', image_create, name='create'),
   path('detail/<int:id>/<slug:slug>/', image_detail, name='detail'),
   path('like/', image_like, name='like')
